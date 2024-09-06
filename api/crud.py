e1b9abeb9591ba2d04ebf4120f62d220dcea9d19
leimber <leimarber@gmail.com>
2024-09-06 09:06:30 +0200

    correccion segunda BD

--- api/crud.py
+++ api/crud.py
@@ -2,26 +2,20 @@ from sqlalchemy.orm import Session
 from . import models_db, schemas_db
 
 def create_passenger_satisfaction(db: Session, passenger: schemas_db.Questions_passenger_satisfactionCreate):
-    """
-    Crea un nuevo registro de satisfacción de pasajero en la base de datos.
-    
-    :param db: Sesión de la base de datos
-    :param passenger: Datos del pasajero a crear
-    :return: El objeto creado
-    """
+   
     db_passenger = models_db.Questions_passenger_satisfaction(**passenger.dict())
-    db.add(db_passenger)  # Añade el nuevo objeto a la sesión
-    db.commit()  # Guarda los cambios en la base de datos
-    db.refresh(db_passenger)  # Actualiza el objeto con datos de la base de datos (ej: id generado)
+    db.add(db_passenger)  
+    db.commit()  
+    db.refresh(db_passenger)  
     return db_passenger
 
 def get_passenger_satisfaction(db: Session, passenger_id: int):
    
-    return db.query(models.Questions_passenger_satisfaction).filter(models.Questions_passenger_satisfaction.id == passenger_id).first()
+    return db.query(models_db.Questions_passenger_satisfaction).filter(models_db.Questions_passenger_satisfaction.id == passenger_id).first()
 
 def get_all_passenger_satisfaction(db: Session, skip: int = 0, limit: int = 100):
   
-    return db.query(models.Questions_passenger_satisfaction).offset(skip).limit(limit).all()
+    return db.query(models_db.Questions_passenger_satisfaction).offset(skip).limit(limit).all()
 
 
 
--- api/main.py
+++ api/main.py
@@ -3,10 +3,9 @@ from sqlalchemy.orm import Session
 from . import crud, models_db, schemas_db
 from .database import engine, get_db
 
-# Crear las tablas en la base de datos
-models.Base.metadata.create_all(bind=engine)
 
-# Crear la aplicación FastAPI
+models_db.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.post("/submit/", response_model=schemas_db.Questions_passenger_satisfaction)
@@ -14,12 +13,12 @@ def submit_form(passenger: schemas_db.Questions_passenger_satisfactionCreate, db
    
     return crud.create_passenger_satisfaction(db=db, passenger=passenger)
 
-@app.get("/passengers/", response_model=list[schemas.Questions_passenger_satisfaction])
+@app.get("/passengers/", response_model=list[schemas_db.Questions_passenger_satisfaction])
 def read_passengers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     passengers = crud.get_all_passenger_satisfaction(db, skip=skip, limit=limit)
     return passengers
 
-@app.get("/passengers/{passenger_id}", response_model=schemas.Questions_passenger_satisfaction)
+@app.get("/passengers/{passenger_id}", response_model=schemas_db.Questions_passenger_satisfaction)
 def read_passenger(passenger_id: int, db: Session = Depends(get_db)):
    
     db_passenger = crud.get_passenger_satisfaction(db, passenger_id=passenger_id)
