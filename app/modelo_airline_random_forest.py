5e4da35d755a9ce95532b90091a0d2c5e8c96e39
angelsc75 <asc7es@gmail.com>
2024-09-06 12:42:35 +0200

    modelo rf con métricas iguales a los demás..

--- app/modelo_airline_random_forest.py
+++ app/modelo_airline_random_forest.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from sklearn.calibration import cross_val_predict
 from sklearn.model_selection import KFold, cross_val_score, GridSearchCV, train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, accuracy_score, roc_curve, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 import joblib
 import seaborn as sns
+from sklearn.utils import shuffle
+import time
 
 
 # Cargar el archivo CSV proporcionado
@@ -12,13 +15,20 @@ file_path = 'data/airline_passenger_satisfaction_model.csv'
 df = pd.read_csv(file_path)
 
 
-# Dividimos el dataset en características (X) y variable objetivo (y)
-X = df.drop(columns=['satisfaction'])
-y = df['satisfaction']
+# Usar una muestra de 10.000 filas para la búsqueda de hiperparámetros
+sample_size = 10000
+df_sample = shuffle(df, random_state=42).iloc[:sample_size]
+
+X_sample = df_sample.drop(columns=['Arrival Delay in Minutes', 'satisfaction'])
+y_sample = df_sample['satisfaction']
+
+# Preparar todo el dataset para el entrenamiento final
+X_full = df.drop(columns=['Arrival Delay in Minutes', 'satisfaction'])
+y_full = df['satisfaction']
 
 
 # Dividir los datos en conjunto de entrenamiento y conjunto de prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X_full, y_full, test_size=0.2, random_state=42, stratify=y_full)
 
 
 # Configurar la validación cruzada con K-Fold
@@ -31,34 +41,46 @@ param_grid_rf = {
     'max_depth': [None, 10, 20, 30],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
-    'max_features': ['auto', 'sqrt', 'log2']
+    'max_features': [None,'sqrt', 'log2']
 }
 
 # Configurar la búsqueda de hiperparámetros para Random Forest
 rf_model = RandomForestClassifier(random_state=42)
+
+# Realizar la búsqueda de hiperparámetros con la muestra
+print("Iniciando búsqueda de hiperparámetros...")
+start_time = time.time()
 grid_search_rf = GridSearchCV(estimator=rf_model, param_grid=param_grid_rf, cv=kf, scoring='accuracy', n_jobs=-1, verbose=2)
 
 # Entrenar el modelo de Random Forest con la búsqueda de hiperparámetros
-grid_search_rf.fit(X_train, y_train)
+grid_search_rf.fit(X_sample, y_sample)
+end_time = time.time()
 
+print(f"Búsqueda de hiperparámetros completada en {end_time - start_time:.2f} segundos")
 # Evaluar el modelo ajustado en el conjunto de test
 best_rf_model = grid_search_rf.best_estimator_
 y_pred_rf = best_rf_model.predict(X_test)
 
-# Imprimir los resultados del modelo Random Forest
-print("Best parameters found for Random Forest:", grid_search_rf.best_params_)
-print("Random Forest Test Accuracy:", accuracy_score(y_test, y_pred_rf))
-print("Random Forest Confusion Matrix:\n", confusion_matrix(y_test, y_pred_rf))
-print("Random Forest Classification Report:\n", classification_report(y_test, y_pred_rf)) # Reporte de clasificación (incluye precision, recall y f1-score)
-print("Random Forest AUC:", roc_auc_score(y_test, best_rf_model.predict_proba(X_test)[:, 1]))
 
-# Cálculo de métricas adicionales
-precision = precision_score(y_test, y_pred_rf, average='binary')  # Cambia 'binary' según el tipo de clasificación
-recall = recall_score(y_test, y_pred_rf, average='binary')
-f1 = f1_score(y_test, y_pred_rf, average='binary')
+
+
+# Evaluar el modelo con validación cruzada
+cv_scores = cross_val_score(best_rf_model, X_train, y_train, cv=5, scoring='accuracy')
+y_pred = cross_val_predict(best_rf_model, X_test, y_test, cv=5)
+y_pred_proba = cross_val_predict(best_rf_model, X_test, y_test, cv=5, method='predict_proba')[:, 1]
+
+# Métricas con pos_label especificado
+conf_matrix = confusion_matrix(y_test, y_pred)
+fpr, tpr, _ = roc_curve(y_test, y_pred_proba, pos_label=1)
+roc_auc = roc_auc_score(y_test, y_pred_proba)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, pos_label=1)
+recall = recall_score(y_test, y_pred, pos_label=1)
+f1 = f1_score(y_test, y_pred, pos_label=1)
+
 
 # Evaluar el modelo utilizando validación cruzada
-cv_scores_rf = cross_val_score(rf_model, X, y, cv=kf, scoring='accuracy')
+cv_scores_rf = cross_val_score(rf_model, X_full, y_full, cv=kf, scoring='accuracy')
 
 # Imprimir los resultados
 print("Random Forest Cross-Validation Accuracy Scores:", cv_scores_rf)
@@ -69,16 +91,7 @@ print("Random Forest Standard Deviation:", cv_scores_rf.std())
 # Plot ROC Curve for Random Forest
 fpr_rf, tpr_rf, _ = roc_curve(y_test, best_rf_model.predict_proba(X_test)[:, 1])
 
-plt.figure()
-plt.plot(fpr_rf, tpr_rf, color='blue', lw=2, label='Random Forest (AUC = %0.2f)' % roc_auc_score(y_test, best_rf_model.predict_proba(X_test)[:, 1]))
-plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc="lower right")
-plt.show()
+
 
 
 # Verificación adicional sobre la precisión del modelo
@@ -89,33 +102,28 @@ print(f"La exactitud del modelo Random Forest es de {accuracy * 100:.2f}%")
 joblib.dump(rf_model, 'models/rf_model.pkl')
 print("Modelo guardado como rf_model.pkl")
 
-#Métricas
-metricsdf = pd.DataFrame({
-    'Model': ['RF'],
-    'Accuracy': [accuracy_score],
+
+
+results = pd.DataFrame({
+    'Model': ['Random Forest'],
+    'Accuracy': [accuracy],
     'Precision': [precision],
     'Recall': [recall],
-    'F1_Score': [f1],
-    'AUC_ROC': [roc_auc_score],
-    'Best_Parameters': [str( grid_search_rf.best_params_)]
+    'F1 Score': [f1],
+    'ROC AUC': [roc_auc],
+    'Best Parameters': [grid_search_rf.best_params_],
 })
+results.to_csv('rf_classification_results.csv', index=False)
 
-#Carga de df
-try:
-    existing_metrics = pd.read_csv('model_metrics.csv')
-    updated_metrics = pd.concat([existing_metrics, metricsdf], ignore_index=True)
-except FileNotFoundError:
-    updated_metrics = metricsdf
-
-updated_metrics.to_csv('model_metrics.csv', index=False)
-print("Métricas guardadas en 'model_metrics.csv'")
-
-# Visualización
-plt.figure(figsize=(10, 6))
-sns.barplot(x=['Accuracy', 'Precision', 'Recall', 'F1_Score', 'AUC_ROC'], 
-            y=[accuracy, precision, recall, f1, roc_auc_score])
-plt.title('Métricas del Modelo Random Forest')
-plt.ylim(0, 1)
-plt.savefig('rf_metrics.png')
+plt.figure()
+plt.plot(fpr_rf, tpr_rf, color='blue', lw=2, label=f'Random Forest (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Curva ROC Random Forest')
+plt.legend(loc="lower right")
+plt.savefig('roc_curve_rf.png')
 plt.close()
-print("Gráfico de métricas guardado como 'rf_metrics.png'")
+
